a5b152fba1e4b141469e402f3b2772d28a80a2b0
Luiggy <luiggyferrin@gmail.com>
2020-12-10 15:43:26 -0500

    chapter 03 added

---
+++ 03. FUNCTIONS, METHODS & OBJECTS/js/area-function.js
@@ -0,0 +1,7 @@
+function getArea(width, height) {
+    var area = width * height;
+     return area;
+}
+
+var wallSize = getArea(5, 5);
+console.log(wallSize);
---
+++ 03. FUNCTIONS, METHODS & OBJECTS/js/basic-function.js
@@ -0,0 +1,6 @@
+var msg = 'Sing up to receive our newsteller for 10% off!';
+function updateMessage() {
+    var el = document.getElementById('message');
+    el.textContent = msg;
+};
+updateMessage();
