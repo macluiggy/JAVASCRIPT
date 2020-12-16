676de5b0a454408c7c3a385432e7c996a9bff640
Luiggy <luiggyferrin@gmail.com>
2020-12-16 09:31:15 -0500

    5 commit

---
+++ 04. DECISIONS & LOOPS/js/for-loop.js
@@ -0,0 +1,16 @@
+var scores = [24, 32, 17];
+var arrayLength = scores.length;
+var roundNumber = 0;
+var msg = '';
+var i;
+
+for (let i = 0; i < arrayLength; i++) {
+    roundNumber = i + 1;
+
+    msg += 'Round ' + roundNumber + ': ';
+
+    msg += scores[i] + '<br>';
+    
+}
+
+document.getElementById('answer').innerHTML = msg;
