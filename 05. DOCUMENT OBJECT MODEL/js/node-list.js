306f6ec0072f8eac59dcff7109cf7321559ef0d7
Luiggy <luiggyferrin@gmail.com>
2020-12-17 10:56:34 -0500

    4 commit

---
+++ 05. DOCUMENT OBJECT MODEL/js/node-list.js
@@ -0,0 +1,8 @@
+var hotItems = document.querySelectorAll('li.hot');
+
+if (hotItems.length > 0) {
+    for (let i = 0; i < hotItems.length; i++) {
+        hotItems[i].className = 'cool';
+        
+    }
+}
