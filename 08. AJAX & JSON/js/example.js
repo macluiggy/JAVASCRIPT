85d102d1d83c8203d08d5222532514a473e2dc5d
Luiggy <luiggyferrin@gmail.com>
2021-01-25 17:27:23 -0500

    chapter 8 finished

--- 08. AJAX & JSON/js/example.js
+++ 08. AJAX & JSON/js/example.js
@@ -19,4 +19,48 @@ $(function () {
     }
 
     loadTimetable();
+
+
+    $('#content').on('click', '#event a', function (e) {
+        e.preventDefault();
+        var loc = this.id.toUpperCase();
+
+        var newContent = '';
+        for (let i = 0; i < times[loc].length; i++) {
+            newContent += '<li><span class="time">' + times[loc][i].time + '</span>';
+            newContent += '<a href="descriptions.html#';
+            newContent += times[loc][i].title.replace(/ /g, '-') + '">';
+            newContent += times[loc][i].title + '</a></li>';
+        }
+
+        $('#sessions').html('<ul>' + newContent + '</ul>');
+
+        $('#event a.current').removeClass('current');
+        $(this).addClass('current');
+
+        $('#details').text('');
+    });
+
+    $('#content').on('click', '#sessions li a', function (e) {
+        e.preventDefault();
+        var fragment = this.href;
+
+        fragment = fragment.replace('#', ' #');
+        $('#details').load(fragment);
+
+        $('#sessions a.current').removeClass('current');
+        $(this).addClass('current');
+    });
+
+    $('nav a').on('click', function (e) {
+        e.preventDefault();
+        var url = this.href;
+
+        $('nav a.current').removeClass('current');
+        $(this).addClass('current');
+
+        $('#container').remove();
+        $('#content').load(url + ' #container').hide().fadeIn('slow');
+    });
+    
 })
