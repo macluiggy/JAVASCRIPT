f7f559a599a8146c476e9d4eb899fc261a325858
Luiggy <luiggyferrin@gmail.com>
2021-01-25 11:34:48 -0500

    3 commit

--- 08. AJAX & JSON/js/jq-get.js
+++ 08. AJAX & JSON/js/jq-get.js
@@ -1,5 +1,6 @@
 var vote = '<div id="vote"><div class="third"><a href="http://example.org?tshirt=gray"><img src="img/t-gray.png" id="gray" alt="gray" /></a></div><div class="third"><a href="http://example.org?tshirt=yellow" id="yellow"><img src="img/t-yellow.png" id="yellow" alt="yellow" /></a></div><div class="third"><a href="http://example.org?tshirt=green"><img src="img/t-green.png" id="green" alt="green" /></a></div><div class"third"><a href="#"><img src="img/home2.jpg" id="home2" alt="home2"></a></div></div>';
 $('#selector').append(vote);
+$('#selector').hide().fadeIn('slow');
 
 $('#selector a').on('click', function (e) {
     e.preventDefault();
